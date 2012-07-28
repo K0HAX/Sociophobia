d44f00ce7ed186433234f61dd9afa3ed3401cc7d
Michael J. Englehorn <michael@xcelor8.com>
2012-07-27 20:05:51 -0500

    Added initial code, and the ability to flow multiple tweets from bottom to top
    Signed-off-by: Michael J. Englehorn <michael@xcelor8.com>

---
+++ Sociophobia/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sociophobia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Tweet tweet = new Tweet("K0HAX", "This is a test! " + i.ToString());
+                uxStackPanel.Children.Insert(0, tweet);
+                tweet.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ Sociophobia/Tweet.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sociophobia
+{
+    /// <summary>
+    /// Interaction logic for Tweet.xaml
+    /// </summary>
+    public partial class Tweet : UserControl
+    {
+        private string m_username;
+        private string m_tweet;
+
+        public Tweet(String username, String tweet)
+        {
+            m_username = username;
+            m_tweet = tweet;
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            uxUsernameText.Content = m_username;
+            uxTweetText.Content = m_tweet;
+        }
+    }
+}
